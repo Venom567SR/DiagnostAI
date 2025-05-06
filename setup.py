da291a695ad3c149da5c3631d31af6b7737d058d
Venom567SR <sahilrahate567@gmail.com>
2025-05-06 18:32:12 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'DiagnostAI',
+    version= '0.0.0',
+    author= "Venom567SR",
+    author_email= "sahilrahate567@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+
+)
